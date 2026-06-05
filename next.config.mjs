6be83b9e4302de5866d426fc51acc764fd3a3218
Nevereverland21 <elegus.15@gmail.com>
2026-06-04 21:32:00 -0500

    msconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import webpack from 'webpack';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -20,23 +22,28 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
 
+    const externals = ['onnxruntime-web', '@imgly/background-removal'];
+
     if (isServer) {
       config.externals = [
         ...(Array.isArray(config.externals) ? config.externals : []),
-        '@imgly/background-removal',
-        'onnxruntime-web',
+        ...externals,
       ];
+    } else {
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp: /^onnxruntime-web$/,
+        })
+      );
     }
 
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        crypto: false,
-        os: false,
-      };
-    }
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      crypto: false,
+      os: false,
+    };
 
     return config;
   },
